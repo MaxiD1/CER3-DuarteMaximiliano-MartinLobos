123c138afeba9b79d228395109de73b0cbbc4406
MaximD1 <maximiliano.duarte@usm.cl>
2025-11-19 21:37:01 -0300

    Ojala ultimas cosas de mi certamen 2, a partir de aquí todo tiene que ser modificado para lo nuevo

--- EcoLibrary/mysite/urls.py
+++ EcoLibrary/mysite/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('eventos/', views.events, name="events"),
+    path('comunidad/', views.community, name="community"),
+    path('registro/', views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('eventos/<int:evento_id>/inscribirse/', views.inscribirse_evento, name='inscribirse_evento'),
+    path('eventos/<int:evento_id>/desinscribirse/', views.desinscribirse_evento, name='desinscribirse_evento'),
+    path('usuario/', views.mis_eventos, name='user'),
     path('admin/', admin.site.urls),
 ]
